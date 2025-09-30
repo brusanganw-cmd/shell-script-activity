af3a8dd8fb88cc7ed965ddf45c8c6206f65e6634
Rwigema Herve <h.rwigema@gmail.com>
2025-09-30 08:08:28 -0700

    final submission

--- vault_monitor.sh
+++ vault_monitor.sh
@@ -1,39 +1,35 @@
-#!/bin/bash 
-
-VAULT_DIR="~/secure_vault.sh"
-REPORT_FILE="$VAULT_DIR/vault_report.txt"
+#!/bin/bash
+VAULT_DIR="$HOME/secure_vault"
+REPORT_FILE="./vault_report.txt"
 
 if [ ! -d "$VAULT_DIR" ]; then
-	echo "ERROR: Directory '$VAULT_DIR' not found. please run vault_setup.sh First"
-	exit 1
-
+    echo "Vault not found."
+    exit 1
 fi
 
-echo "--- Vauly Security report ---" > "$REPORT_FILE"
-echo "Generated on: $(date)" >> "$REPORT_FILE"
-echo "-----------------------" >> "$REPORT_FILE"
+{
+    echo "=== Vault Report ==="
+    date
+    echo
 
-for file in "$VAULT_DIR"/*; do
-	if [ -f "$file" ]; then
-		file_name=$(basename "$file")
-		size_bytes=$(stat -c %s "$file")
-		last_modifies=$(stat -c %y "$file")
-		permissions_octal=$(stat -c %a "$file")
+    for file in "$VAULT_DIR"/*; do
+        fname=$(basename "$file")
+        size=$(stat -c%s "$file")
+        modified=$(stat -c%y "$file")
+        perms=$(stat -c%A "$file")
+        numeric_perms=$(stat -c%a "$file")
 
-		echo "File Name: $file_name" >> "$REPORT_FILE"
-		echo "Size bytes: $size_bytes" >> "$REPORT_FILE"
-		echo "Last Modified: $last_modified" >> "$REPORT_FILE"
-		echo "Permissions: $permissions_octal" >> "$REPORT_FILE"
+        echo "File: $fname"
+        echo "Size: $size bytes"
+        echo "Last Modified: $modified"
+        echo "Permissions: $perms ($numeric_perms)"
 
-		if [ "$permissions_octal" -gt 644 ]; then
-			echo "SECURITY RISK DETECTED: Permissions are too open!" >> "$REPORT_FILE"
+        if [ "$numeric_perms" -gt 644 ]; then
+            echo " SECURITY RISK DETECTED"
+        fi
 
-		fi
+        echo "---------------------------"
+    done
+} > "$REPORT_FILE"
 
-		echo "-----------------------------------------" >> "$REPORT_FILE"
-
-	fi
-done
-
-echo "Vault monitoring complete"
-echo "Report has been generated at '$REPORT_FILE'"
+echo "Vault report generated at $REPORT_FILE"
